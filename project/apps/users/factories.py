66b009a2f56adf6e1d7c8a95264bd1ca9c5016d5
Max Kharandziuk <max.kharandziuk@gmail.com>
2014-10-29 23:29:34 +0200

    added PostFactory

--- project/apps/users/factories.py
+++ project/apps/users/factories.py
@@ -16,3 +16,11 @@ class UserFactory(factory.DjangoModelFactory):
     def _create(cls, model_class, *args, **kwargs):
         manager = cls._get_manager(model_class)
         return manager.create_user(*args, **kwargs)
+
+
+class PostFactory(factory.DjangoModelFactory):
+
+    FACTORY_FOR = models.User
+
+    title = factory.Sequence(lambda i: "title{}".format(i))
+    body = factory.Sequence(lambda i: "body{}".format(i))
